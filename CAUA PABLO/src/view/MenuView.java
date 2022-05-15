5ca8373277298325af850eee6b39169b7068c701
Cauã Pablo <cauapxd1@gmail.com>
2022-05-15 18:19:33 -0300

    Revert "Alteração nos Atributos do Model"
    This reverts commit c99d8a906a7864e351bd1bbded494b7682123470.

--- CAUA PABLO/src/view/MenuView.java
+++ CAUA PABLO/src/view/MenuView.java
@@ -7,60 +7,43 @@ public class MenuView {
 
         System.out.println("****BEM VINDO AO MENU****");
         System.out.println("____________________________________________");
-        System.out.println(" 1-Cadastrar Usuario \n " +
-                "2-Cadastrar Fornecedor \n " +
-                "3-Cadastrar Tipo de Pessoa \n " +
-                "4-Cadastrar Tipo \n " +
-                "5-Cadastrar Cliente" +
-                "\n 6-Cadastrar Produto \n" +
-                " 0-Sair\n");
-
-        System.out.println("***Qual Opção Você Deseja?***");
+        System.out.println(" 1-Cadastrar Usuario \n 2-Cadastrar Fornecedor \n 3-Cadastrar Tipo de Pessoa \n 4-Cadastrar Tipo \n 5-Cadastrar Cliente" +
+                "\n 6-Cadastrar Produto \n 0-Sair");
+        System.out.println("____________________________________________\n");
+        System.out.println("Qual Opção Você Deseja?");
         int escolhaDoUsuario = ler.nextInt();
 
         if (escolhaDoUsuario == 1){
-            System.out.println("***Opção Escolhida: Cadastrar Usuario***");
+            System.out.println("Opção Escolhida: Cadastrar Usuario");
             UsuarioView user1 = new UsuarioView();
             user1.menuDoUsuario(null);
         }
-
         else if(escolhaDoUsuario == 2){
-            System.out.println("***Opção Escolhida: Cadastrar Fornecedor***");
-            FornecedorView forn1 = new FornecedorView();
-            forn1.CadForn(null);
-        }
+            System.out.println("Opção Escolhida: Cadastrar Fornecedor");
+            UsuarioView user1 = new UsuarioView();
+            user1.CadForn(null);
 
+        }
         else if(escolhaDoUsuario == 3){
-            System.out.println("***Opção Escolhida: Cadastrar Tipo de Pessoa***");
-            TipoPessoaView tipopessoa1 = new TipoPessoaView();
-            tipopessoa1.CadTipoPessoa(null);
+            System.out.println("Opção Escolhida: Cadastrar Tipo de Pessoa");
         }
-
         else if(escolhaDoUsuario == 4){
-            System.out.println("***Opção Escolhida: Cadastrar Tipo de Produto***");
-            TipoProdutoView tipoProd = new TipoProdutoView();
-            tipoProd.CadTipoProd();
+            System.out.println("Opção Escolhida: Cadastrar Tipo de");
         }
-
         else if(escolhaDoUsuario == 5){
-            System.out.println("***Opção Escolhida: Cadastrar Cliente***");
-            ClienteView cli1 = new ClienteView();
-            cli1.CadCliente(null);
+            System.out.println("Opção Escolhida: Cadastrar Cliente");
         }
-
         else if(escolhaDoUsuario == 6){
-            System.out.println("***Opção Escolhida: Cadastrar Produto***");
-            ProdutoView prod1 = new ProdutoView();
-            prod1.CadProduto(null);
+            System.out.println("Opção Escolhida: Cadastrar Produto");
         }
-
         else if(escolhaDoUsuario == 0){
-            System.out.println("***Obrigado Por Utilizar Nossos Serviços!***");
+            System.out.println("Obrigado Por Utilizar Nossos Serviços!");
+
+        }
+        else if(escolhaDoUsuario > 7 || escolhaDoUsuario < 0){
+            System.out.println("O Número Informado é Inválido!");
         }
 
-        else if(escolhaDoUsuario > 7 || escolhaDoUsuario < 0){
-            System.out.println("***O Número Informado é Inválido!***");
-        }
     }
 
 
