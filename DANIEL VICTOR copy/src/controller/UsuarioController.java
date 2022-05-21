843610e1c4adcf87634f529796f00dbf0c0bc072
daniel.costa <daniel.costa@tron.com.br>
2022-05-20 21:45:52 -0300

    Teste

--- DANIEL VICTOR copy/src/controller/UsuarioController.java
+++ DANIEL VICTOR copy/src/controller/UsuarioController.java
@@ -1,4 +1,5 @@
 package controller;
+
 import java.util.ArrayList;
 
 import model.Usuario;
@@ -6,45 +7,42 @@ import service.UsuarioService;
 
 public class UsuarioController {
 
-	
-	public boolean cadastrar(Usuario usuario){
-		 UsuarioService usuarioService = new UsuarioService();
-		  
-		 if (usuarioService.ler(usuario)) {	 
-          System.out.println("Já possui Cadastro");
-	         return true;
-	       }
-		 else {
-		  usuarioService.escrever(usuario);	 
-		  System.out.println("Cadastro realizado com sucesso!");
-	         return false;
-	       }	
-	}
-	
-	public void atualizar(Usuario usuario) {
-		
-	}
-	
-	public void deletar(Usuario usuario) {
-		
-	}
-	
-	public Usuario listar(String listar) {
-		Usuario usuario = new Usuario();
-		
+	public boolean cadastrar(Usuario usuario) {
 		UsuarioService usuarioService = new UsuarioService();
-		return usuarioService.ler(listar);
-		
-	}
-	
-	public ArrayList<Usuario> listar(){
-		
-		ArrayList<Usuario> listaUsuario = new ArrayList<Usuario>();
-		
-		UsuarioService usuarioService = new UsuarioService();
-		
-		return usuarioService.ler();
+
+		if (usuarioService.ler(usuario)) {
+			System.out.println("Jï¿½ possui Cadastro");
+			return true;
+		} else {
+			usuarioService.escrever(usuario);
+			System.out.println("Cadastro realizado com sucesso!");
+			return false;
+		}
 	}
 
+	public void atualizar(Usuario usuario) {
+
+	}
+
+	public void deletar(Usuario usuario) {
+
+	}
+
+	public Usuario listar(String listar) {
+		Usuario usuario = new Usuario();
+
+		UsuarioService usuarioService = new UsuarioService();
+		return usuarioService.ler(listar);
+
+	}
+
+	public ArrayList<Usuario> listar() {
+
+		ArrayList<Usuario> listaUsuario = new ArrayList<Usuario>();
+
+		UsuarioService usuarioService = new UsuarioService();
+
+		return usuarioService.ler();
+	}
 
 }
